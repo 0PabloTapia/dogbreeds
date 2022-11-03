484437f0fb5b4ff2ad7a2bed8e971eacfe7de18c
pablo.tapia <pablo.tapia@i-car.cl>
2022-11-03 15:32:44 -0300

    🎉First Commit!

---
+++ src/Home.jsx
@@ -0,0 +1,11 @@
+import DogList from "./components/DogList";
+
+const Home = () => {
+  return (
+    <>
+      <DogList />
+    </>
+  );
+};
+
+export default Home;
---
+++ src/components/DogList.jsx
@@ -0,0 +1,14 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const DogList = () => {
+  
+
+  return (
+    <>
+      <p>Dogs</p>
+    </>
+  );
+};
+
+export default DogList;
