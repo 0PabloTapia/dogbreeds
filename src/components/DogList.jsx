9efe917320ed9d7eff0b30e5c629c169ac7a0cb1
pablo.tapia <pablo.tapia@i-car.cl>
2022-11-05 13:02:38 -0300

    ✨Feat/render and listed dogs by breeds and subbreeds + treeSelect

--- src/components/DogList.jsx
+++ src/components/DogList.jsx
@@ -1,8 +1,11 @@
 import { useEffect, useState } from "react";
+import { TreeSelect } from "antd";
+import { Card } from "antd";
 import axios from "axios";
 
 const DogList = () => {
   const [allBreeds, setAllBreeds] = useState([]);
+  const [valuesByBreed, setValuesByBreed] = useState([]);
 
   useEffect(() => {
     const url = "https://dog.ceo/api/breeds/list/all";
@@ -16,13 +19,93 @@ const DogList = () => {
       });
   }, []);
 
-  
+  const onChange = (breeds) => {
+    console.log("onChange ", breeds);
+    const randomId = Math.floor(Math.random() * Math.floor(Math.random() * Date.now()));
+    const lastSelectedBreed = breeds.slice(-1);
+    const subBreedValues = [].concat(...Object.values(allBreeds));
+    const itsSubBreed = subBreedValues.includes(lastSelectedBreed.toString());
+    const breedPics = [];
+    if (itsSubBreed) {
+      axios
+        .get(`https://dog.ceo/api/breed/${lastSelectedBreed}/images`)
+        .then((res) => {
+          const breedImages = res.data.message;
+          for (let image of breedImages) {
+            breedPics.push({ id: randomId, dog: image });
+          }
+          setValuesByBreed((prevBreedPics) => [...prevBreedPics, ...breedPics]);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } else {
+      const breedsURL = `https://dog.ceo/api/breed/${lastSelectedBreed}/images`;
+      axios
+        .get(breedsURL)
+        .then((res) => {
+          const breedImages = res.data.message;
+          for (let image of breedImages) {
+            breedPics.push({ id: randomId, dog: image });
+          }
+          setValuesByBreed((prevBreedPics) => [...prevBreedPics, ...breedPics]);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  };
+
+  console.log("values", allBreeds);
+
+  const { SHOW_PARENT } = TreeSelect;
+
+  const tProps = {
+    onChange,
+    treeCheckable: true,
+    showCheckedStrategy: SHOW_PARENT,
+    placeholder: "Please select",
+    style: {
+      width: "30%",
+    },
+  };
+
+  const gridStyle = {
+    width: "20%",
+    padding: "4rem 2rem 4rem 2rem",
+    marginTop: "1rem",
+    marginLeft: "4.5rem",
+    textAlign: "center",
+    backgroundColor: "#DE2151",
+  };
+
+  const handleTreeData = () => {
+    return Object.entries(allBreeds).map(([breed, subBreed], i) => ({
+      title: breed,
+      value: breed,
+      children: subBreed.map((x, i) => {
+        return {
+          title: x,
+          value: x,
+        };
+      }),
+    }));
+  };
+
+  console.log('byBreed', valuesByBreed)
 
   return (
     <>
-      {Object.keys(allBreeds).map((x, i) => {
-        return <p key={i}>{x}</p>;
-      })}
+      <TreeSelect {...tProps} treeData={handleTreeData()} />
+      <Card>
+        {valuesByBreed.map((x, i) => {
+          return (
+            <Card.Grid style={gridStyle} key={i}>
+              <Card cover={<img src={x.dog} alt={"dog_image"} />}></Card>
+            </Card.Grid>
+          );
+        })}
+      </Card>
     </>
   );
 };
